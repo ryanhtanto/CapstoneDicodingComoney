3f59e7dfbe024aa8b49ab47e6600989d6411c706
ryanhtanto <ryanherrtanto@gmail.com>
2022-11-17 10:22:41 +0700

    login page and font settings

--- comoney-capstone/src/index.js
+++ comoney-capstone/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../src/styles/general.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- comoney-capstone/src/pages/LoginPage.jsx
+++ comoney-capstone/src/pages/LoginPage.jsx
@@ -1,18 +1,38 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import React from 'react';
 
 function LoginPage() {
         return (
-                <section>
-                        <div className='container'>
+                <section id="login" className='overflow-hidden'>
+                        {/* <div className='container'> */}
                                 <div className='row'>
-                                        <article className='col-lg-6'>
-
+                                        <article className='col-lg-6 bg-auth'>
+                                                <div className='container d-flex flex-column min-vh-100 justify-content-center'>
+                                                        <h3 className='text-center fw-bold text-white'>Track your money!</h3>
+                                                        <p className='text-center text-white'>Do all that in just one simple app</p>
+                                                        <img src="../../public/images/Currency Crush Graphs.png" alt="login-image" srcSet="" />
+                                                </div>
                                         </article>
-                                        <div className='col-lg-6'>
-                                                
+                                        <div className='col-lg-6 d-flex flex-column min-vh-100 justify-content-center px-5 container'>
+                                                <h4 className='fw-bold'>Log In</h4>
+                                                <p className='font-color'>Welcome back! Please enter your account</p>
+                                                <form>
+                                                        <div className="form-group">
+                                                                <input type="email" className="form-control" id="email" aria-describedby="emailHelp" placeholder='Email'/>
+                                                        </div>
+                                                        <div className="form-group mt-4">  
+                                                                <input type="password" className="form-control" id="password" placeholder='Password'/>
+                                                        </div>
+                                                        <div className="form-group mt-4">  
+                                                                <button type="button" className="btn btn-color col-12 text-white">Login</button>
+                                                        </div>
+                                                        <div className="form-group mt-4">  
+                                                                <p className='text-center font-color'>Don't have an account?   <a href="" className='linkedAuth fw-bold'>Sign Up</a></p>
+                                                        </div>
+                                                </form>
                                         </div>
                                 </div>
-                        </div>
+                        {/* </div> */}
                 </section>
         );
 }
