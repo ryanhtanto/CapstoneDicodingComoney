48589f200f91838c09e580b9ca67f6e797047737
vincentt14 <vincent14@student.umn.ac.id>
2022-11-23 15:31:56 +0700

    adding delete category

--- comoney-capstone/src/components/AddTransactionForm.jsx
+++ comoney-capstone/src/components/AddTransactionForm.jsx
@@ -3,16 +3,24 @@ import { FiPlusSquare } from "react-icons/fi";
 import AddNewCategoryDropdown from "./AddNewCategoryDropdown";
 import AddNewCategoryModal from "./AddNewCategoryModal";
 import useInput from "../hooks/UseInput";
+import { FiTrash2 } from "react-icons/fi";
+import newCategoryIdb from "../data/new-category-idb";
 
 const AddIncomeForm = () => {
   const [name, setName] = useInput("");
   const [amount, setAmount] = useInput("");
   const [selectedCategory, setSelectedCategory] = React.useState("");
+  const [categoryId, setCategoryId] = React.useState("");
   const [description, setDescription] = useInput("");
 
   const onSubmit = (e) => {
     e.preventDefault();
-    console.log(name, amount, selectedCategory ,description);
+    console.log(name, amount, selectedCategory, categoryId, description);
+  };
+
+  const onDelete = () => {
+    newCategoryIdb.deleteCategory(categoryId);
+    window.location.reload();
   };
 
   return (
@@ -23,16 +31,27 @@ const AddIncomeForm = () => {
 
         <div className="text-center my-4">
           <div className="row">
-            <div className="col-sm-12 col-lg-9 mb-2">
+            <div className="col-sm-12 col-lg-8 mb-2">
               <div className="dropdown">
-                <AddNewCategoryDropdown placeHolder="Select Category" categoryCallback={(value) => {setSelectedCategory(value.data)}}/>
+                <AddNewCategoryDropdown
+                  placeHolder="Select Category"
+                  categoryCallback={(value) => {
+                    setSelectedCategory(value.data);
+                    setCategoryId(value.id);
+                  }}
+                />
               </div>
             </div>
 
-            <div className="col">
+            <div className="col-sm-8 col-lg-3 mb-2">
               {/* button trigger modal */}
               <button type="button" className="btn btn-primary form-control btn-color input__height" data-bs-toggle="modal" data-bs-target="#exampleModal">
                 <FiPlusSquare /> New Category
+              </button>
+            </div>
+            <div className="col-sm-4 col-lg-1">
+              <button type="submit" className="btn btn-danger form-control input__height btn-hapus" onClick={() => onDelete()}>
+                <FiTrash2 />
               </button>
             </div>
           </div>
