02b7f68c8c2cb6308025d5a68fab6003a87ab954
ryanhtanto <ryanherrtanto@gmail.com>
2022-11-28 15:50:43 +0700

    savings dikit

--- comoney-capstone/src/components/SavingPlanItem.jsx
+++ comoney-capstone/src/components/SavingPlanItem.jsx
@@ -76,7 +76,7 @@ function SavingPlanItem({ saving, onDelete }) {
           <h6 className="mt-2">
             <FiCheckSquare />
             <span className="mx-2">{locale === "en" ? 'Save' : 'Tabung'} Rp {roundedAmount} / {locale === 'en' ? 'month' : 'bulan'}</span>
-            { isOneMonth === true ? <p className="mb-0 mt-1 reminderOneMonth fw-bold ">{locale === "en" ? 'Left on month' : 'Tersisa satu bulan'}</p> : '' }
+            { isOneMonth === true ? <p className="mb-0 mt-1 reminderOneMonth fw-bold ">{locale === "en" ? 'Left one month!' : 'Tersisa satu bulan!'}</p> : '' }
           </h6>
           <Link to={`/edit-saving-plan/${saving.id}`}>
             <EditSavingButton />
