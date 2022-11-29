325ab4797809edb6482526edcdd4e026a3762c58
imfaditya <imamaditya2211@gmail.com>
2022-11-29 09:58:43 +0700

    install service worker & update manifest

--- comoney-capstone/src/index.js
+++ comoney-capstone/src/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
 import './styles/general.css';
@@ -18,6 +19,7 @@ root.render(
   </BrowserRouter>,
 );
 
+serviceWorkerRegistration.register();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
