2b9c221a62b43f36734d68cbfd0ce99e71242811
imfaditya <imamaditya2211@gmail.com>
2022-12-04 18:56:23 +0700

    use env

--- src/global/api-config.js
+++ src/global/api-config.js
@@ -1,11 +1,11 @@
 const NEWS_API = {
   BASE_URL: 'https://gnews.io/api/v4/search',
-  KEY: 'ceed8ce1d8fd00e55ea5e3bf1280122e',
+  KEY: process.env.REACT_APP_NEWS_API_KEY,
 };
 
 const QUOTES_API = {
   BASE_URL: 'https://api.api-ninjas.com/v1/quotes',
-  KEY: '23BDtggAeCK4iKtwod18LQ==y3mSSV3gUoa0l8GJ',
+  KEY: process.env.REACT_APP_QUOTES_API_KEY,
 };
 
 export { NEWS_API, QUOTES_API };
